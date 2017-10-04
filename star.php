268e892591dbdbcafa40e57ad37b2830a8d92ec4
kishoreasudani <kishore.asudani@wedigtech.com>
2017-10-04 17:21:57 +0530

    Nishant Mathur Commit

--- star.php
+++ star.php
@@ -24,4 +24,37 @@ for($i=5;$i>=1;$i--)
 	echo "<br>";
 }
 
+
+function print_parttern_two($n){
+	$r = ($n-1);
+
+	for ($i=1; $i <= $n; $i++) { 
+		for ($s=$r; $s >= 1 ; $s--) { 
+			echo "&nbsp;";
+		}
+		for ($j=1; $j <= $i ; $j++) { 
+			echo "*";
+		}
+		echo "<br/>";
+		$r--;
+	}
+
+
+	$r = abs($r);	
+
+	for ($i=$n; $i >= 1; $i--) { 
+		for ($s=1; $s < $r ; $s++) { 
+			echo "&nbsp;";
+		}
+		for ($j=$i; $j >= 1 ; $j--) { 
+			echo "*";
+		}
+		echo "<br/>";
+		$r++;
+	}
+
+}
+
+print_parttern_two(5);
+
 echo "Vishal"; ?>
