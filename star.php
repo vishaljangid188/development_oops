04710f3045465cf5f9325bfe9445ca80691311c9
Kishore <kishore.asudani@wedigtech.com>
2017-10-04 12:09:16 +0530

    first commit

---
+++ star.php
@@ -0,0 +1,26 @@
+<?php
+for($i=0;$i<=6;$i++)
+{
+	for($k=6;$k>=$i;$k--)
+	{
+		echo "  ";
+	}
+	for($j=1;$j<=$i;$j++)
+	{
+		echo "*  ";
+	}
+	echo "<br>";
+}
+for($i=5;$i>=1;$i--)
+{
+	for($k=6;$k>=$i;$k--)
+	{
+		echo "  ";
+	}
+	for($j=1;$j<=$i;$j++)
+	{
+		echo "*  ";
+	}
+	echo "<br>";
+}
+?>
